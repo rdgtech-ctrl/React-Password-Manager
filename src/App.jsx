509db00c328f0c2ae44d3e0976bcd7dd3e031b1b
Disha <dg.mechpro200@gmail.com>
2025-12-23 21:12:41 +0530

    Auto commit: 2025-12-23 21:12:40

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
+import Manager from './components/Manager'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <Navbar />
-      <div>my ui</div>
+      <Manager />
     </>
   )
 }
